3e71c7041a29dabbe6438523c025480e93ca4601
tangweikun <819573105@qq.com>
2016-06-23 20:29:03 +0800

    demo3

--- demo3/client/components/App.js
+++ demo3/client/components/App.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Footer from './Footer.js'
 import AddTodo from '../containers/AddTodo.js'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
-const App = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
-)
+// const App = () => (
+//   <div>
+//     <AddTodo />
+//     <VisibleTodoList />
+//     <Footer />
+//   </div>
+// )
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+      </div>
+    )
+  }
+}
 
 export default App
