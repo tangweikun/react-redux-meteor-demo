3944d77a003c75467cd1a08138bd5afc928ba088
tangweikun <819573105@qq.com>
2016-06-30 10:31:36 +0800

    demo8

---
+++ demo8/client/main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createStore } from 'redux'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import todoApp from './reducers'
+import App from './components/App'
+
+let store = createStore(todoApp)  //from reducers
+
+$(document).ready(function() {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>, document.getElementById('root')
+  )
+})
